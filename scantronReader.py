78c15c3a50f89e720fb79498a23851e2a922ccc8
Jared Gregor <32880933+jmgregor@users.noreply.github.com>
2020-04-23 23:53:20 -0400

    updated GUI

--- scantronReader.py
+++ scantronReader.py
@@ -12,27 +12,44 @@ import Scantron
 
 def BrowseFiles(): 
     global filename
+    global label2
+    global label3
     filename = filedialog.askopenfilename(initialdir = "/", 
                                           title = "Select a File", 
                                           filetypes = (("Pdf files", 
                                                         "*.pdf*"), 
                                                        ("all files", 
                                                         "*.*")))   
-    print("\nLoading file: " + filename)
+    print(filename + " loaded.")
+    label3 = tk.Label(win,text="Loaded:")
+    label3.place(x=0,y=180)  
+    label2 = tk.Label(win,text = filename)
+    label2.place(x=50,y=180)
     return filename
         
 #function to be called when the grading button is pressed
 def ComputeGrades(filename):
     print("\nGrading...\n")
+    label3.destroy()
+    label2.destroy()
+    label4 = tk.Label(win,text="Grading...")
+    label4.place(x=0,y=180)
     Scantron.ScantronGrades(filename)
-    print("\nAll done!  :)")
+    label4.destroy()
+    label5 = tk.Label(win,text = "Grading Complete! GradedScantrons.csv created.")
+    label5.place(x=0,y=180)
+    print("All done. :)")
 
 
 #create instance of tkinter
 win = tk.Tk()
 
 #sets the size of the window
-win.geometry("300x100")
+win.geometry("400x200")
+
+photo = tk.PhotoImage(file=r"C:\Users\Aidan Herbert\Desktop\mini3.png")
+label = tk.Label(win,image=photo)
+label.place(x=40,y=0)
 
 #add a title to the window
 win.title("Grading Tool")
@@ -41,14 +58,12 @@ win.title("Grading Tool")
 win.resizable(False,False)   
 
 button1 = tk.Button(win,text="Load Scantrons", bd = 5, activeforeground = "green", activebackground = "lightgrey")
-button1.place(x = 50,y = 50)
+button1.place(x = 70,y = 140)
 button1['command'] = BrowseFiles
 
-button2 = tk.Button(win,text="Grade!",bd = 5, activeforeground = "green", activebackground = "lightgrey")
-button2.place(x=200,y=50)
+button2 = tk.Button(win,text="Grade Scantrons",bd = 5, activeforeground = "green", activebackground = "lightgrey")
+button2.place(x=220,y=140)
 button2['command'] =lambda: ComputeGrades(filename)
-
-
 
 #infinite gui main loop
 #required for any gui application to work
